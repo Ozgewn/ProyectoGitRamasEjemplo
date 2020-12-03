83539c37026bf74aa8f036d6b84ffe5aaea0665c
Ozgewn <albert.machio@iesnervion.es>
2020-12-03 14:49:22 +0100

    Pues otro commit más para ver si me deja de dar error

--- src/ProyectoGitRamasEjemplo.java
+++ src/ProyectoGitRamasEjemplo.java
@@ -4,11 +4,6 @@ public class ProyectoGitRamasEjemplo {
 	public static void main(String[] args) {
 
 		System.out.println("Hell0 mundo");
-
+		
 	}
-	
-	public static void imprimeSaludo{
-		System.out.println("Hei");
-	}
-
 }
