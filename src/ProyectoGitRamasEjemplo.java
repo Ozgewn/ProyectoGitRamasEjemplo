b18261f910f1e4ca3211e8e0019286b27896d0a7
Ozgewn <albert.machio@iesnervion.es>
2020-12-03 14:01:07 +0100

    Commit con push después de commit sin push

--- src/ProyectoGitRamasEjemplo.java
+++ src/ProyectoGitRamasEjemplo.java
@@ -2,9 +2,14 @@
 public class ProyectoGitRamasEjemplo {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("Hello mundo");
 
 	}
+
+	public static void imprimeSaludo() {
+		System.out.println("Hei");
+	}
+
 
 }
