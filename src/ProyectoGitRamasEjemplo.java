231c3eaa3da0ba3c5f33fa812ae53190110f4d2a
Ozgewn <albert.machio@iesnervion.es>
2020-12-03 14:02:59 +0100

    Volvemos al principio porque me lo he hecho mal xd

--- src/ProyectoGitRamasEjemplo.java
+++ src/ProyectoGitRamasEjemplo.java
@@ -3,13 +3,8 @@ public class ProyectoGitRamasEjemplo {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello mundo");
+		System.out.println("Hola mundo");
 
 	}
-
-	public static void imprimeSaludo() {
-		System.out.println("Hei");
-	}
-
 
 }
